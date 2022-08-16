404eb4adbfeb306d99734aafbda3d46190a21ae9
Wuyupei <495174699@qq.com>
2022-08-16 18:20:54 +0800

    fix 登录过期处理

--- src/store/detial/index.js
+++ src/store/detial/index.js
@@ -1,6 +1,7 @@
 import { defineStore } from 'pinia';
 import Message from '../../components/Message';
 import { requestDetial } from '../../service/detial/index';
+import useHomeStore from '../home/index';
 
 const useDetialStore = defineStore('detial', {
   state() {
@@ -16,6 +17,9 @@ const useDetialStore = defineStore('detial', {
       if (res.data.code == 200) {
         this.shopDetial = res.data.data;
       } else if (res.data.data.code == 401) {
+        const homeStore = useHomeStore();
+        homeStore.userimg = '';
+        homeStore.token = '';
         router.replace('/');
         new Message({ message: '登录已过期,请重新登录' });
       }
