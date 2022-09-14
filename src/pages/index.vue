8e7a2c4f288cf8ff16868491d3179b90db37df5e
Wuyupei <495174699@qq.com>
2022-09-14 09:53:33 +0800

    add message

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,7 +2,7 @@
   <div class="home" md:pt-70 pt-50>
     <Wrapper>
       <template #w-slot>
-        <div class="wrapper-home" flex justify-center items-start flex-wrap  c-white>
+        <div class="wrapper-home" flex justify-center items-start flex-wrap c-white>
           <h2>资源小站</h2>
           <div>助你成为卷王<sup>之王</sup></div>
         </div>
@@ -22,6 +22,8 @@ meta:
 import Wrapper from '../components/Wrapper.vue';
 import Category from '../components/Category.vue';
 import Content from '../components/Content.vue';
+import Message from '../components/Message';
+new Message({ message: '个人练习项目,数据均为模拟数据,请勿相信！' });
 </script>
 
 <style scoped>
@@ -43,6 +45,6 @@ import Content from '../components/Content.vue';
 .wrapper-home div sup {
   font-size: 30px;
   font-weight: 800;
-  color:red;
+  color: red;
 }
 </style>
